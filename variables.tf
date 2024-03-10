a3bfbd71e20c7ec0d6616858ccf6baaa5609dcd1
Tauqeer Ahmad <hellotauqeer@gmail.com>
2024-03-10 23:59:37 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "vm_size" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "Standard_D2s_v3"
+}
