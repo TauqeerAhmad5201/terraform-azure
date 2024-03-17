28accb2b36d4d1b6123099c00e8ab1fb31a7c4de
Tauqeer Ahmad <hellotauqeer@gmail.com>
2024-03-17 17:22:03 +0530

    updated backend.tf to use

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "my-ubuntu-rg"
-    storage_account_name = "storage"
-    container_name       = "TFbackend"
-    key                  = "prod.terraform.tfstate"
+    resource_group_name  = "backend-terra-test"
+    storage_account_name = "storagetauqeer007"
+    container_name       = "tfstate"
+    key                  = "app-service.tfstate"
   }
 }
